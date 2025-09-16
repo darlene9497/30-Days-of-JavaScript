071a1657c6f8935fa07969e1cb53b83251476d42
darlene9497 <nyamburadarlene6974@gmail.com>
2025-09-16 16:50:49 +0300

    :art:add initial JavaScript files for data types and variables, completing day 1 of 30

---
+++ introduction/datatypes.js
@@ -0,0 +1,10 @@
+const isString = "Introduction";
+const isBoolean = true;
+let isUndefined;
+let isNull = null;
+
+// check type of data
+console.log(typeof isString);
+console.log(typeof isBoolean);
+console.log(typeof isUndefined);
+console.log(typeof isNull);
---
+++ introduction/variable.js
@@ -0,0 +1,33 @@
+// comments can make code readable
+// Welcome to 30DaysOfJavaScript
+
+/** comments can make code readable, 
+easy to reuse and informative */
+
+const isString = "Introduction";
+const isBoolean = true;
+let isUndefined;
+let isNull = null;
+
+// variables without assigning values
+let varA, varB, varC, varD;
+
+// variables with assigning values
+let varE = "JavaScript";
+let varF = 28;
+let varG = true;
+let varH = [20, 19, 45];
+
+const firstName = "Darlene";
+const lastName = "Nyambura";
+const status = "single";
+const country = "Kenya";
+const age = 30;
+
+const myfirstName = "Darlene", mylastName = "Nyambura", mystatus = "single", mycountry = "Kenya", myage = 30;
+
+const myAge = 25;
+const yourAge = 30;
+
+console.log(`I am ${myAge} years old`);
+console.log(`You are ${yourAge} years old`);
